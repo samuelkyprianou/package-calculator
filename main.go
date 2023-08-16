54b1f74ae0a39f13b85bb6c8a026f178f5e30502
SamKyprianou <samuel.kyprianou@digital.justice.gov.uk>
2023-08-16 12:24:27 +0100

    add http API and order form UI

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
+func PacksHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	orderQuantityStr := query.Get("order_quantity")
+	orderQuantity, err := strconv.Atoi(orderQuantityStr)
+
+	if err != nil {
+		http.Error(w, "Invalid order quantity", http.StatusBadRequest)
+		return
+	}
+
+	packsNeeded := CalculatePacks(orderQuantity)
+	response := "<h1>Calculated Packs:</h1>\n"
+	for size, count := range packsNeeded {
+		if count > 0 {
+			response += fmt.Sprintf("<p>%d x %d</p>\n", count, size)
+		}
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(response))
 }
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "order_form.html")
+	})
+	http.HandleFunc("/calculate_packs", PacksHandler)
+	fmt.Println("Server started at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
--- pack_calculation.go
+++ pack_calculation.go
@@ -1,5 +1,11 @@
 package main
 
-func CalculatePacks(oQuantity int) int {
-return oQuantity
+func CalculatePacks(oQuantity int) map[int]int {
+	return map[int]int{
+		250:  0,
+		500:  0,
+		1000: 0,
+		2000: 0,
+		5000: 0,
+	}
 }
